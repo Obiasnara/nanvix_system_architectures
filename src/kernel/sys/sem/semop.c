d9b7bff0eb31f935597d595c4902c6c5c2c014ae
Nicolas Picaud <nicolas.picaud@etu.univ-savoie.fr>
2024-02-14 13:02:30 +0100

    Everything OK

--- src/kernel/sys/sem/semop.c
+++ src/kernel/sys/sem/semop.c
@@ -1,26 +1,36 @@
 #include <sys/sem.h>
 #include <nanvix/pm.h>
-
+#include <nanvix/hal.h>
 #ifdef SEM_ENABLED
 int up(int semid) {
+    disable_interrupts();
     struct sem *sem = &sems[semid];
-    if (!(sem->valid)) return -1; // Skip invalid semaphore
+    if (!(sem->valid)) {
+        enable_interrupts();
+        return -1; // Skip invalid semaphore
+    }
     if (sem->n <= 0 && sem->sleep_chain) {
         wakeup_one(&(sem->sleep_chain));
     } else {
         sem->n++;
     }
+    enable_interrupts();
     return 0;
 }
 
 int down(int semid) {
+    disable_interrupts();
     struct sem *sem = &sems[semid];
-    if (!(sem->valid)) return -1; // Skip invalid semaphore
+    if (!(sem->valid)) {
+        enable_interrupts();
+        return -1; // Skip invalid semaphore
+    }
     if (sem->n <= 0) {
         sleep(&(sem->sleep_chain), curr_proc->priority);
     } else {
         sem->n--;
     }
+    enable_interrupts();
     return 0;
 }
 
